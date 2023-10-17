53bf06433cd5d82b1681f60edac760ccef7c8115
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2023-10-18 02:27:47 +0300

    change baseURL Axios instance

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,10 +1,10 @@
-import axios from "axios";
+import axios from "axios"
 
 const instance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
-  headers: { Accept: "application/json" },
-  withCredentials: true,
-  timeout: 5000,
-});
+    baseURL: import.meta.env.DEV ? import.meta.env.VITE_API_URL : process.env.APP_API_URL,
+    headers: { Accept: "application/json" },
+    withCredentials: true,
+    timeout: 5000,
+})
 
-export default instance;
+export default instance
