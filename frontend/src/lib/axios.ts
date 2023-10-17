ba60ef4297f02b5ac76f43c58ae3f699f7f29f94
Hasbiy Robbiy <hasbii.rofficial@gmail.com>
2023-10-18 02:34:15 +0300

    change axios instance again

--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: import.meta.env.DEV ? import.meta.env.VITE_API_URL : process.env.APP_API_URL,
+    baseURL: import.meta.env.DEV ? import.meta.env.VITE_API_URL : "https://rm.has-bii.online/api",
     headers: { Accept: "application/json" },
     withCredentials: true,
     timeout: 5000,
